768f7326f4788249a047abad83498a8d968fb679
unknown <patrick.dahlke@dev-pc-patrick.contoso.int>
2014-11-21 13:23:23 +0100

    vector constructor

--- vector.go
+++ vector.go
@@ -6,3 +6,13 @@ type vector struct {
 
 	// I NEED A FUCKING CONSTRUCTUR
 }
+
+//constructor
+//see: https://golang.org/doc/effective_go.html#composite_literals
+func NewVector(x, y, z int32) *vector {
+	v := new(vector)
+
+	//more stuff here
+
+	return v
+}
