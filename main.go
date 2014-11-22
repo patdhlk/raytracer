e8c9e0a6cf35e45f619e950b665746910321df16
unknown <patrick.dahlke@dev-pc-patrick.contoso.int>
2014-11-22 17:44:54 +0100

    just the two for loops

--- main.go
+++ main.go
@@ -1,1 +1,18 @@
 package raytracer
+
+import (
+	"log"
+)
+
+func main() {
+	log.Println("rendering")
+	width = 640
+	height = 480
+
+	//starts from left side of the image for each pixel to the right side of the image
+	for x := 0; x < width; x++ {
+		for i := 0; i < height; i++ {
+
+		}
+	}
+}
