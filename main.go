073dfa2cca32789cbaf4a2e4c9bc32e3b62c75f7
unknown <patrick.dahlke@dev-pc-patrick.contoso.int>
2014-11-24 10:59:14 +0100

    main bug fix backgroundworker core num implementation

--- main.go
+++ main.go
@@ -4,10 +4,22 @@ import (
 	"log"
 )
 
+// TODO pseudocode for basic ray tracer
+// :for each screen pixel
+// :generate a ray from the camera to the pixel
+// :intersect the ray with all objects in the scene
+// :for the closest intersection
+// :determine the material
+// :for each light in the scene
+// :generate a ray from the intersection to the light
+// :         if not obstructed: Apply illumination
+// :spawn secondary rays (e.g., reflection, refraction)
+// :combine results
+
 func main() {
 	log.Println("rendering")
-	width = 640
-	height = 480
+	width := 640
+	height := 480
 
 	//starts from left side of the image for each pixel to the right side of the image
 	for x := 0; x < width; x++ {
