493d487e4fcbb0da798108b5f80828b7f65ee0c2
cproduct <heier.micha@web.de>
2014-11-24 21:54:59 +0100

    plane test
    minimum test of plane...

---
+++ plane_test.go
@@ -0,0 +1,25 @@
+package raytracer
+
+import "testing"
+
+func TestPlaneFindIntersection(t *testing.T) {
+	p := NewPlane(*NewVector(0, 0, 0), *NewVector(0, 1, 0), 0.0)
+
+	t1 := p.findIntersection(*NewRay(*NewVector(0, 0, 0), *NewVector(0, -1, 0)))
+
+	if t1 != 0.0 {
+		t.Errorf("TestPlaneFindIntersection1 %v", t1)
+	}
+
+	t1 = p.findIntersection(*NewRay(*NewVector(0, 1, 0), *NewVector(0, -1, 0)))
+
+	if t1 != 1.0 {
+		t.Errorf("TestPlaneFindIntersection2 %v", t1)
+	}
+
+	t1 = p.findIntersection(*NewRay(*NewVector(0, -1, 0), *NewVector(0, -1, 0)))
+
+	if t1 != -1.0 {
+		t.Errorf("TestPlaneFindIntersection3 %v", t1)
+	}
+}
