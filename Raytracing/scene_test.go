28a397fd43162b3393456bb84b9c772af15644df
Patrick Dahlke <patrick.dahlke@pichu.io>
2014-12-09 11:13:59 +0100

    same as before

--- Raytracing/scene_test.go
+++ Raytracing/scene_test.go
@@ -15,8 +15,16 @@ func TestNewScene(t *testing.T) {
 	g := NewGrid(objects.NewVector(1.0, 2.0, 3.0), objects.NewVector(1.0, 5.0, 6.0))
 	s := NewScene(objects.NewVector(7.0, 8.0, 9.0), g)
 
-	if *s.View() != *objects.NewVector(7.0, 8.0, 9.0) || *s.Grid().TopLeft() != *objects.NewVector(1.0, 2.0, 3.0) || *s.Grid().BottomRight() != *objects.NewVector(1.0, 5.0, 6.0) || s.Elements() == nil || *s.Ambient() != *objects.NewVector(0.0, 0.0, 0.0) || *s.SkyColor() != *objects.NewVector(0.0, 0.0, 0.0) || s.Light() == nil {
-		t.Errorf("TestNewScene %v %v %v %v %v %v %v", s.View(), s.Grid().TopLeft(), s.Grid().BottomRight(), s.Elements(), s.Ambient(), s.SkyColor(), s.Light())
+	if *s.View() != *objects.NewVector(7.0, 8.0, 9.0) ||
+		*s.Grid().TopLeft() != *objects.NewVector(1.0, 2.0, 3.0) ||
+		*s.Grid().BottomRight() != *objects.NewVector(1.0, 5.0, 6.0) ||
+		s.Elements() == nil ||
+		*s.Ambient() != *objects.NewVector(0.0, 0.0, 0.0) ||
+		*s.SkyColor() != *objects.NewVector(0.0, 0.0, 0.0) ||
+		s.Light() == nil {
+		t.Errorf("TestNewScene %v %v %v %v %v %v %v", s.View(), s.Grid().TopLeft(),
+			s.Grid().BottomRight(), s.Elements(),
+			s.Ambient(), s.SkyColor(), s.Light())
 	}
 
 	g = NewGrid(objects.NewVector(1.0, 2.0, 3.0), objects.NewVector(2.0, 5.0, 6.0))
@@ -27,8 +35,16 @@ func TestSceneGetSet(t *testing.T) {
 	g := NewGrid(objects.NewVector(1.0, 2.0, 3.0), objects.NewVector(1.0, 5.0, 6.0))
 	s := NewScene(objects.NewVector(7.0, 8.0, 9.0), g)
 
-	if *s.View() != *objects.NewVector(7.0, 8.0, 9.0) || *s.Grid().TopLeft() != *objects.NewVector(1.0, 2.0, 3.0) || *s.Grid().BottomRight() != *objects.NewVector(1.0, 5.0, 6.0) || s.Elements() == nil || *s.Ambient() != *objects.NewVector(0.0, 0.0, 0.0) || *s.SkyColor() != *objects.NewVector(0.0, 0.0, 0.0) || s.Light() == nil {
-		t.Errorf("TestSceneGetSet 1 %v %v %v %v %v %v %v", s.View(), s.Grid().TopLeft(), s.Grid().BottomRight(), s.Elements(), s.Ambient(), s.SkyColor(), s.Light())
+	if *s.View() != *objects.NewVector(7.0, 8.0, 9.0) ||
+		*s.Grid().TopLeft() != *objects.NewVector(1.0, 2.0, 3.0) ||
+		*s.Grid().BottomRight() != *objects.NewVector(1.0, 5.0, 6.0) ||
+		s.Elements() == nil || *s.Ambient() != *objects.NewVector(0.0, 0.0, 0.0) ||
+		*s.SkyColor() != *objects.NewVector(0.0, 0.0, 0.0) ||
+		s.Light() == nil {
+		t.Errorf("TestSceneGetSet 1 %v %v %v %v %v %v %v", s.View(), s.Grid().TopLeft(),
+			s.Grid().BottomRight(),
+			s.Elements(), s.Ambient(),
+			s.SkyColor(), s.Light())
 	}
 
 	g = NewGrid(objects.NewVector(10.0, 11.0, 12.0), objects.NewVector(10.0, 14.0, 15.0))
@@ -39,7 +55,15 @@ func TestSceneGetSet(t *testing.T) {
 	s.SetAmbient(objects.NewVector(19.0, 20.0, 21.0))
 	s.SetSkyColor(objects.NewVector(22.0, 23.0, 24.0))
 
-	if *s.View() != *objects.NewVector(16.0, 17.0, 18.0) || *s.Grid().TopLeft() != *objects.NewVector(10.0, 11.0, 12.0) || *s.Grid().BottomRight() != *objects.NewVector(10.0, 14.0, 15.0) || s.Elements() == nil || *s.Ambient() != *objects.NewVector(19.0, 20.0, 21.0) || *s.SkyColor() != *objects.NewVector(22.0, 23.0, 24.0) {
-		t.Errorf("TestSceneGetSet 2 %v %v %v %v %v %v %v", s.View(), s.Grid().TopLeft(), s.Grid().BottomRight(), s.Elements(), s.Ambient(), s.SkyColor(), s.Light())
+	if *s.View() != *objects.NewVector(16.0, 17.0, 18.0) ||
+		*s.Grid().TopLeft() != *objects.NewVector(10.0, 11.0, 12.0) ||
+		*s.Grid().BottomRight() != *objects.NewVector(10.0, 14.0, 15.0) ||
+		s.Elements() == nil ||
+		*s.Ambient() != *objects.NewVector(19.0, 20.0, 21.0) ||
+		*s.SkyColor() != *objects.NewVector(22.0, 23.0, 24.0) {
+		t.Errorf("TestSceneGetSet 2 %v %v %v %v %v %v %v", s.View(), s.Grid().TopLeft(),
+			s.Grid().BottomRight(), s.Elements(),
+			s.Ambient(), s.SkyColor(),
+			s.Light())
 	}
 }
